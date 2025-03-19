5f7f4f9c222fe9a0385d49d629df86eff2ef6171
kushalbillur <kushalbillur@klebcahubli.in>
2025-03-19 15:56:56 +0530

    third change

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const router = require("./routes/authRoutes");
 const cartrouter = require("./routes/cartRoutes");
 connectDB()
 const app= express();
-
-app.use(cors())
+const allowedOrigins = ["ecom-frontend-nizu0dm1p-kushal-bs-projects.vercel.app"
+    ,"ecom-frontend-iota-weld.vercel.app"]
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allows cookies and authentication headers if needed
+  }))
 
 app.use(express.json())
 
